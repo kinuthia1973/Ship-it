b9d70f93a291d7dc7ae75a46dc00cc06736af8b2
kinuthia1973 <kinuthiamuiruri46@gmail.com>
2026-03-03 09:08:05 -0500

    Create system-info.js

---
+++ system-info.js
@@ -0,0 +1,5 @@
+console.log("System Information Script Running...");
+console.log("Node version:", process.version);
+console.log("Platform:", process.platform);
+console.log("Architecture:", process.arch);
+console.log("Current directory:", process.cwd());
